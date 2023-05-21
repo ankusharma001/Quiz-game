168055e137d7553fa79fede592c3d61742ec6598
ankita21202 <122442776+ankita21202@users.noreply.github.com>
2023-05-21 22:42:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const myButton = document.getElementById("incorrect");
+const body =document.body;
+const colors = [ 'red']
+
+button.addEventListener('click', chageBackground)
+function changeBackground(){
+  body.style.backgroundColor = colors[colorsIndex]
+}
